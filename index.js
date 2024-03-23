1f77b67f24a547782523314508110cf853b32ee6
Collins Ngetich <collinsngetichk@gmail.com>
2024-03-24 00:38:55 +0300

    modfies index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression 'divide'
+function divide(a, b) {
+  return a / b;
+}
+
+// Arrow function 'square'
+const square = (x) => {
+  return x * x;
+};
+
+// Arrow function 'add'
+const add = (a, b) => {
+  return a + b;
+};
